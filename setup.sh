24542138f6ab9e0d69776d7a16a6fdffd14bf4e1
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-16 16:18:35 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,55 @@
+
+
+pkg up -y
+pkg upgrade -y
+apt update -yapt upgrade -y
+apt dist-upgrade -y
+apt full-upgrade -y
+dpkg --configure -a
+apt --fix-broken install -y
+apt install --fix-missing -y
+
+termux-setup-storage
+
+pkg install 
+
+apksigner
+android-tools
+aapt2
+
+sudo tsu tudo
+
+bat
+zoxide zsh zsh-completions
+antibody/stable
+
+micro
+mc yazi ranger lf fm 
+abduco cfm
+clifm
+cfm
+termux-api
+termux-gui-package
+binutils-libs
+binutils-bin
+binutils-is-llvm
+
+wget aria2c
+zip unzip
+
+ripgrep-all
+
+pkgtop
+
+libheif libwebp 
+optipng pngquant jpegoptim
+graphicsmagick imagemagick
+go-findimagedupes cavif-rs
+gifsicle gifski
+
+
+tur-repo
+
+apt clean
+apt autoclean && 
+apt-get -y autoremove --purge
