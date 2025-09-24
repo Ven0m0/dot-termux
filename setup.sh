c7b6d425353b16eeb8c6f7555c95bd0aa8bda81c
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-24 11:16:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+termux-setup-storage
+
 pkg up -y
 pkg upgrade -y
 apt update -yapt upgrade -y
@@ -9,11 +11,13 @@ dpkg --configure -a
 apt --fix-broken install -y
 apt install --fix-missing -y
 
-pkg install termux-api termux-gui-package tur-repo -y
-pkg install wget aria2c zip unzip -y
-termux-setup-storage
 
-pkg install git zsh zsh-completions -y
+pkg install termux-api termux-gui-package tur-repo -y
+
+pkg install wget aria2c zip unzip git -y
+pkg install micro mc  -y
+
+zsh zsh-completions -y
 chsh -s zsh
 git clone https://github.com/Sohil876/Termux-zsh.git && cd Termux-zsh && bash setup.sh
 
@@ -30,3 +34,6 @@ pkg install libheif libwebp optipng pngquant jpegoptim gifsicle gifski imagemagi
 
 apt clean && apt autoclean && apt-get -y autoremove --purge
 
+
+curl -fsSL https://raw.githubusercontent.com/ashish0kumar/fzfm/main/fzfm -o ~/.local/bin/fzfm
+chmod +x ~/.local/bin/fzfm
