6aac564c30a692520690741f25d4ed4575bc65b4
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-25 06:31:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,32 +11,39 @@ dpkg --configure -a
 apt --fix-broken install -y
 apt install --fix-missing -y
 
-
-pkg install termux-api termux-gui-package tur-repo -y
-
-pkg install wget aria2c zip unzip git -y
-pkg install micro mc  -y
+pkg install -y termux-api tur-repo 
+pkg install -y wget aria2 zip unzip git 
+pkg install -y micro mc 
+pkg install -y fzf 
 
 zsh zsh-completions -y
 chsh -s zsh
+
+termux-reload-settings
+
 git clone https://github.com/Sohil876/Termux-zsh.git && cd Termux-zsh && bash setup.sh
 
-pkg install android-tools aapt2 apksigner -y
-pkg install bat zoxide ripgrep-all -y
-pkg install micro mc -y
+fc-cache -vf
+
+#pkg install -y android-tools aapt2 apksigner -y
+pkg install -y bat zoxide ripgrep ripgrep-all -y
 #binutils-libs
 #binutils-bin
 #binutils-is-llvm
 #pkgtop
 
-pkg install libheif libwebp optipng pngquant jpegoptim gifsicle gifski imagemagick -y
+pkg in -y nala 
+
+pkg in -y uv tealdeer 
+
+#pkg install libheif libwebp optipng pngquant jpegoptim gifsicle gifski imagemagick -y
 #graphicsmagick go-findimagedupes cavif-rs
 
 apt clean && apt autoclean && apt-get -y autoremove --purge
 
-
-curl -fsSL https://raw.githubusercontent.com/ashish0kumar/fzfm/main/fzfm -o ~/.local/bin/fzfm
+mkdir -p "${HOME}/.local/bin"
+curl -fsSL https://raw.githubusercontent.com/ashish0kumar/fzfm/main/fzfm -o "${HOME}/.local/bin/fzfm"
 chmod +x ~/.local/bin/fzfm
 
-curl https://raw.githubusercontent.com/CodesOfRishi/navita/main/navita.sh --output navita.sh
+curl https://raw.githubusercontent.com/CodesOfRishi/navita/main/navita.sh -o "${HOME}/navita.sh"
 
