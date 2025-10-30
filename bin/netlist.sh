709eef901e31fbf27610dd933ce4463140a55cef
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-10-30 06:49:09 +0100

    Update netlist.sh

--- bin/netlist.sh
+++ bin/netlist.sh
@@ -11,7 +11,6 @@ curl -s wttr.in/$location?0
 # Speedtest DL/UP
 down=$(curl -s -o /dev/null -w "%{speed_download}" https://speed.cloudflare.com/__down?bytes=100000000)
 awk -v s="$down" 'BEGIN {printf "Download: %.2f Mbps\n", (s*8)/(1024*1024)}'
-
 up=$(dd if=/dev/zero bs=1M count=10 2>/dev/null | \
   curl -s -o /dev/null -w "%{speed_upload}" --data-binary @- https://speed.cloudflare.com/__up)
 awk -v s="$up" 'BEGIN {printf "Upload: %.2f Mbps\n", (s*8)/(1024*1024)}'
