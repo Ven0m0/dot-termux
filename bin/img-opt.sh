c594b4d6c1c55eb1485d5d0d01b1c90436216904
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-05 04:47:16 +0100

    Update img-opt.sh

--- bin/img-opt.sh
+++ bin/img-opt.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 # Wrapper for image optimization.
-fdjpg(){ fd . "${1:-.}" -e jpg -e jpeg -x jpegoptim -s --all-progressive -m85; }
-fdpng(){ fd . "${1:-.}" -e png -x optipng -o2 -strip all -fix -clobber; }
 # Usage: source img-opt.sh; fdwebp [dir] OR ./img-opt.sh [dir]
 set -euo pipefail
 export LC_ALL=C
 # Dependencies: pkg install fd libwebp
 
+# TODO: 
+# fdjpg(){ fd . "${1:-.}" -e jpg -e jpeg -x jpegoptim -s --all-progressive -m85; }
+# fdpng(){ fd . "${1:-.}" -e png -x optipng -o2 -strip all -fix -clobber; }
+
 # WebP: Convert JPG/PNG to WebP (q80) and DELETE original
 fdwebp(){
   # bash -c allows chaining '&& rm'; ${1%.*} strips ext
   fd . "${1:-.}" -e jpg -e jpeg -e png \
-    -x bash -c 'cwebp -q 80 -quiet "$1" -o "${1%.*}.webp" && rm "$1"' _ "{}"
+    -x bash -c 'cwebp -q 80 -m 6 -pass 10 -quiet -metadata none -mt \
+    "$1" -o "${1%.*}.webp" && rm "$1"' _ "{}"
 }
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
   TARGET="${1:-.}"
