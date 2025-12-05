da419be0473c7e3a027a9bdbdf9ddbd84f5bd709
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-05 06:49:33 +0100

    Update img-webp.sh

--- bin/img-webp.sh
+++ bin/img-webp.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 set -euo pipefail; shopt -s nullglob globstar
-IFS=$'\n\t'; export LC_ALL=C LANG=C DEBIAN_FRONTEND=noninteractive
+IFS=$'\n\t' LC_ALL=C LANG=C DEBIAN_FRONTEND=noninteractive
 # Wrapper for image optimization.
 # Usage: source img-opt.sh; fdwebp [dir] OR ./img-opt.sh [dir]
 # Dependencies: pkg install fd libwebp
@@ -10,7 +10,7 @@ fdwebp(){
   # bash -c allows chaining '&& rm'; ${1%.*} strips ext
   fd -tf -e jpg -e jpeg -e png -E '*.webp' . "${1:-.}" \
     -x bash -c 'cwebp -q 80 -m 6 -pass 10 -quiet -metadata none \
-    "$1" -o "${1%.*}.webp" && rm "$1"' _ "{}"
+    "$1" -o "${1%.*}.webp" && rm "$1"' _ "{}" || :
 }
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
   TARGET="${1:-.}"
