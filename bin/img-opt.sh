d5090ff6a9edb2112c3680b25b395106b9b03f26
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-05 06:50:36 +0100

    Create img-opt.sh

---
+++ bin/img-opt.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/env bash
+set -euo pipefail; shopt -s nullglob globstar
+IFS=$'\n\t' LC_ALL=C LANG=C DEBIAN_FRONTEND=noninteractive
+
+img-opt(){
+  if command -v image-optimizer &>/dev/null; then
+    image-optimizer --png-optimization-level max -r --zopfli-iterations "${2:-50}" --max-size 1920 -i "${1:-.}" || :
+  fi
+}
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  TARGET="${1:-.}"
+  echo "🚀 Optimizing images in '$TARGET'..."
+  img-opt "$TARGET"
+  echo "✅ Done."
+fi
