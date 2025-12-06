625c4eac8a9ce9c17816daed512b8dac4f3c2d04
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-06 05:28:36 +0100

    Update img-opt.sh

--- bin/img-opt.sh
+++ bin/img-opt.sh
@@ -1,15 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/env bash
-set -euo pipefail; shopt -s nullglob globstar
-IFS=$'\n\t' LC_ALL=C LANG=C DEBIAN_FRONTEND=noninteractive
+set -euo pipefail; shopt -s nullglob globstar; IFS=$'\n\t'; LC_ALL=C; LANG=C; DEBIAN_FRONTEND=noninteractive
 
-img-opt(){
-  if command -v image-optimizer &>/dev/null; then
-    image-optimizer --png-optimization-level max -r --zopfli-iterations "${2:-50}" --max-size 1920 -i "${1:-.}" || :
-  fi
-}
+has(){ command -v -- "$1" &>/dev/null; }
+img-opt(){ has image-optimizer && image-optimizer --png-optimization-level max -r --zopfli-iterations "${2:-50}" --max-size "${3:-1920}" -i "${1:-.}" || :; }
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  TARGET="${1:-.}"
-  echo "Optimizing images in '$TARGET'..."
+  TARGET="${1:-.}"; echo "Optimizing images in '$TARGET'..."
   img-opt "$TARGET"
   echo "Done."
 fi
