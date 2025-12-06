0c90c9d90a14195fdd43781cad831f2e7b1e516e
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-06 10:11:47 +0100

    Create setup_mise.sh

---
+++ setup_mise.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/env bash
+set -eo pipefail; shopt -s nullglob globstar; IFS=$'\n\t'; export LC_ALL=C; LANG=C; DEBIAN_FRONTEND=noninteractive
+
+pkg update -ym && pkg i -y wget curl gpg
+install -dm 755 /data/data/com.termux/files/usr/etc/apt/keyrings
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | tee /data/data/com.termux/files/usr/etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
+echo "deb [signed-by=/data/data/com.termux/files/usr/etc/apt/keyrings/mise-archive-keyring.gpg arch=arm64] https://mise.jdx.dev/deb stable main" | tee /data/data/com.termux/files/usr/etc/apt/sources.list.d/mise.list
+pkg update -y && pkg install -y mise
