b1b543e6b91aae272f52cb308750efc35052a09c
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-10 03:49:00 +0100

    Update termux-fix-shebang.sh

--- bin/termux-fix-shebang.sh
+++ bin/termux-fix-shebang.sh
@@ -1,11 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/sh
+# shellcheck enable=all shell=bash source-path=SCRIPTDIR external-sources=true
 # Usage: termux-fix-shebang <files>
 # Optimization: Parallel processing with xargs
-
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 file1 [file2 ...]"
-  exit 1
+  echo "Usage: $0 file1 [file2 ...]"; exit 1
 fi
-
 # Use xargs to batch-process files with sed
 printf '%s\0' "$@" | xargs -0 sed -i -E "1 s@^#\!(.*)/[sx]?bin/(.*)@#\!/data/data/com.termux/files/usr/bin/\2@"
