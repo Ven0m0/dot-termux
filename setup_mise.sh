04226c45e1636786dbe64868ff30f594b28b2215
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-10 03:49:47 +0100

    Update setup_mise.sh

--- setup_mise.sh
+++ setup_mise.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/env bash
-set -eo pipefail; shopt -s nullglob globstar; IFS=$'\n\t'; export LC_ALL=C; LANG=C; DEBIAN_FRONTEND=noninteractive
-
+# shellcheck enable=all shell=bash source-path=SCRIPTDIR external-sources=true
+set -eo pipefail; shopt -s nullglob globstar; IFS=$'\n\t'; export LC_ALL=C; DEBIAN_FRONTEND=noninteractive
 pkg update -ym && pkg i -y wget curl gpg
 install -dm 755 /data/data/com.termux/files/usr/etc/apt/keyrings
 wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | tee /data/data/com.termux/files/usr/etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
